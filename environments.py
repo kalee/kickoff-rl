8e0e48ab90649e42a9a888c2f1f44145458ff34d
Philip N. Brown <philipnbrown@gmail.com>
2025-06-02 15:21:05 -0600

    initial

---
+++ environments.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  2 15:15:50 2025
+
+@author: phili
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class GridWorld :
+    def __init__(self,width) :
+        self.width = width
+        
+        
